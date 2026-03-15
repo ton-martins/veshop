6bd31f110e33cce39255cd5b22ab27cdb5da8de7
Everton Martins <everton.junior@dynatecnologia.io>
2026-03-14 23:42:03 -0300

    🎨 headers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use RuntimeException;
 
@@ -33,6 +34,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->assertSafeTestingDatabase();
+
+        if ((bool) config('app.force_https', false)) {
+            URL::forceScheme('https');
+
+            $appUrl = rtrim((string) config('app.url', ''), '/');
+            if ($appUrl !== '') {
+                URL::forceRootUrl($appUrl);
+            }
+        }
 
         Vite::prefetch(concurrency: 3);
     }
