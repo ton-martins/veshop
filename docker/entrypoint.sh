fc356ec25c4218f9542d38948533251d39b2c28e
Everton Martins <everton.junior@dynatecnologia.io>
2026-03-18 17:29:37 -0300

    🎨 storage no deploy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,15 @@ set -e
 mkdir -p /var/www/storage/framework/cache
 mkdir -p /var/www/storage/framework/sessions
 mkdir -p /var/www/storage/framework/views
+mkdir -p /var/www/storage/app/public
 mkdir -p /var/www/bootstrap/cache
 
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+
+if [ ! -L /var/www/public/storage ]; then
+    rm -rf /var/www/public/storage
+    php artisan storage:link >/dev/null 2>&1 || true
+fi
 
 php-fpm -D
 
